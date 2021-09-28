eeb05a48182a39362bdf27753438b538599da6a2
MichelleCoffey <michellembcoffey@icloud.com>
2021-09-28 13:21:06 +0000

    add profile page.

--- app.py
+++ app.py
@@ -41,9 +41,10 @@ def register():
         }
         mongo.db.users.insert_one(register)
 
-        #put the new user into session cookie
+        # put the new user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Yay... You're in. Time to start adding those family recipes.")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -57,9 +58,12 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -72,6 +76,13 @@ def login():
 
     return render_template("login.html")
 
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 
