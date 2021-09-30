1464239ddefd7c0854b41fe51a5c624ad86d0fd0
MichelleCoffey <michellembcoffey@icloud.com>
2021-09-30 11:51:28 +0000

    Allow user to add recipe to db.

--- app.py
+++ app.py
@@ -99,8 +99,23 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    if request.method == "POST":
+        is_not_gluten_free = "off" if request.form.get(
+            "is_not_gluten_free") else "on"
+        recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_ingredients": request.form.get("recipe_ingredients"),
+            "recipe_method": request.form.getlist("recipe.method"),
+            "is_not_gluten_free": is_not_gluten_free,
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Sucessfully Added")
+        return redirect(url_for('get_recipes'))
+        
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipe.html", categories=categories)
 
